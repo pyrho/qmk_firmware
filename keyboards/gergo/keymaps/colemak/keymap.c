78241d3ea2d29f6a807ffc6c6634375af47965ae
Damien Rajon <145502+pyrho@users.noreply.github.com>
2019-04-23 00:29:11 +0200

    addded macros

--- keyboards/gergo/keymaps/colemak/keymap.c
+++ keyboards/gergo/keymaps/colemak/keymap.c
@@ -16,7 +16,12 @@
 enum custom_keycodes {
   M1_STRING = SAFE_RANGE,
   M2_URL,
+  M3_KWTEST,
+  M4_ALT_TAB
 };
+
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
 
 // Blank template at the bottom
 
@@ -48,14 +53,14 @@ KC_LSFT,              KC_Z,  KC_X,   KC_C,   KC_V, KC_B, OSM(MOD_LALT),       KC
 
                                      ALT_T(KC_DEL), KC_BSPC,                           KC_SPC, CMD_T(KC_DEL),
 
-                                                         KC_BTN3,       KC_COLON,
+                                                         KC_BTN3,       M4_ALT_TAB,
                                 LT(SYMB, KC_ESC), LT(NUMB, KC_F13),     LT(NUMB, KC_F14), LT(SYMB, KC_ENT)),
 /* Keymap 1: Symbols layer
  *
  * ,-------------------------------------------.                         ,-------------------------------------------.
  * |        |  !   |  @   |  #   |  $   |  %   |                         |   ^  |  &   |  *   |  (   |  )   |  VolUp |
  * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
- * |        |  [   |  ]   |  {   |  }   |  `   |  M1  |           |      |      |  -   |  _   |  +   |  =   |  VolDn |
+ * |        |  [   |  ]   |  {   |  }   |  `   |  M1  |           |  M3  |      |  -   |  _   |  +   |  =   |  VolDn |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
  * |        |  `   |  ~   |      |      |  ~   |  M2  |           |      |      |      | Prev |Pl/Pau| Next |  Mute  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -71,9 +76,9 @@ KC_LSFT,              KC_Z,  KC_X,   KC_C,   KC_V, KC_B, OSM(MOD_LALT),       KC
  *                                 `--------------'                     `--------------'
  */
 [SYMB] = LAYOUT_GERGO(
-KC_TRNS, KC_EXLM, KC_AT,  KC_HASH,KC_DLR, KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC__VOLUP,
-KC_TRNS, KC_LBRC, KC_RBRC, KC_LCBR,KC_RCBR, KC_PLUS, M1_STRING,             KC_TRNS,  KC_TRNS, KC_MINS, KC_UNDERSCORE, KC_PLUS, KC_EQL, KC__VOLDOWN,
-KC_TRNS, KC_GRV, KC_TILD,KC_TRNS,KC_TRNS, KC_EQL, M2_URL,                 KC_TRNS,  KC_TRNS, KC_TRNS,  KC_MEDIA_REWIND, KC_MEDIA_PLAY_PAUSE,  KC_MEDIA_FAST_FORWARD, KC__MUTE,
+KC_TRNS, KC_EXLM, KC_AT,  KC_HASH,KC_DLR, KC_PERC,                                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC__VOLUP,
+KC_TRNS, KC_LBRC, KC_RBRC, KC_LCBR,KC_RCBR, KC_PLUS, M1_STRING,             M3_KWTEST, KC_TRNS, KC_MINS, KC_UNDERSCORE, KC_PLUS, KC_EQL, KC__VOLDOWN,
+KC_TRNS, KC_GRV, KC_TILD,KC_TRNS,KC_TRNS, KC_EQL, M2_URL,                   KC_TRNS,  KC_TRNS, KC_TRNS,  KC_MEDIA_REWIND, KC_MEDIA_PLAY_PAUSE,  KC_MEDIA_FAST_FORWARD, KC__MUTE,
 
                                                   KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS,
 
@@ -104,7 +109,7 @@ KC_TRNS, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                             K
 KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_BTN1,         KC_HOME,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END, KC_PGDN,
 KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_BTN2,        KC_TRNS,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS,
 
-                                                  KC_TRNS, KC_TRNS,       KC_RALT, KC_TRNS,
+                                                  KC_TRNS, KC_TRNS,       RALT(KC_RSFT), KC_TRNS,
 
                                                            KC_TRNS,       KC_TRNS,
                                              	  KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS)
@@ -148,6 +153,12 @@ void matrix_init_user(void) {
 void matrix_scan_user(void) {
     //uint8_t layer = biton32(layer_state);
     biton32(layer_state);
+    if (is_alt_tab_active) {
+      if (timer_elapsed(alt_tab_timer) > 500) {
+        unregister_code(KC_LCMD);
+        is_alt_tab_active = false;
+      }
+    }
 };
 
 
@@ -155,16 +166,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case M1_STRING:
       if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("Hi!" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKBEST is released
+        SEND_STRING("Azerty12" SS_TAP(X_ENTER));
       }
       break;
 
     case M2_URL:
       if (record->event.pressed) {
-          SEND_STRING("https://ddg.gg" SS_TAP(X_ENTER));
+          SEND_STRING(SS_LGUI("t") "https://bit.ly/dlwebtest" SS_TAP(X_ENTER));
+      }
+      break;
+
+    case M3_KWTEST:
+      if (record->event.pressed) {
+        // Input login
+        SEND_STRING("kw_test_ut_dra2018_1@mailinator.com" SS_LGUI("a") SS_LGUI("c") SS_TAP(X_ENTER));
+        _delay_ms(2000);
+
+        // Get the token
+        SEND_STRING(SS_TAP(X_F13));
+        _delay_ms(500);
+        SEND_STRING("gettoken" SS_TAP(X_ENTER));
+        _delay_ms(500);
+
+        // Paste it
+        SEND_STRING(SS_LGUI("v") SS_TAP(X_ENTER));
+        _delay_ms(2000);
+
+        // Input pw
+        SEND_STRING("Azerty12" SS_TAP(X_ENTER));
+      }
+      break;
+
+      case M4_ALT_TAB:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          register_code(KC_LCMD);
+        }
+        alt_tab_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
       }
       break;
 
